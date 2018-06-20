3ecd5a8c8cc1684b902182eb3177c86e607d6096
sheucm <st0502123@gmail.com>
2018-06-20 14:10:45 +0800

    modify inference code and refine all code

--- car_detection_for_video.py
+++ car_detection_for_video.py
@@ -1,81 +1,20 @@
-
-# coding: utf-8
-
-# # Object Detection Demo
-# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
-
-# # Imports
-
-# In[98]:
-
-
+import time
 import numpy as np
 import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
-
-from PIL import Image
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-from object_detection.utils import ops as utils_ops
-
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
-
-
-# ## Env setup
-
-# In[99]:
-
-
 import cv2
 
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[100]:
-
-
 from object_detection.utils import label_map_util
-
-#from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import visualization_utils as vis_util
 from visualization_utils import visualize_boxes_and_labels_on_image_array
 from tracker import TrackerHandler
 from counter import Counter
 
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[101]:
-
-
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = 'graph/mobilenet_v1/frozen_inference_graph.pb'
-
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('', 'object-detection.pbtxt')
-
 NUM_CLASSES = 1
-
-
-# ## Download Model
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-# In[102]:
 
 
 detection_graph = tf.Graph()
@@ -86,119 +25,85 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[103]:
-
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
-# ## Helper code
-
-# In[104]:
-
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
-# # Detection
-
-# In[105]:
-
-
-def run_inference_for_single_image(image, graph):
-  with graph.as_default():
-    with tf.Session() as sess:
-      # Get handles to input and output tensors
-      ops = tf.get_default_graph().get_operations()
-      all_tensor_names = {output.name for op in ops for output in op.outputs}
-      tensor_dict = {}
-      for key in [
-          'num_detections', 'detection_boxes', 'detection_scores',
-          'detection_classes', 'detection_masks'
-      ]:
+# Get tensors from graph
+with detection_graph.as_default():
+    ops = tf.get_default_graph().get_operations()
+    all_tensor_names = {output.name for op in ops for output in op.outputs}
+    tensor_dict = {}
+    for key in [
+      'num_detections', 'detection_boxes', 'detection_scores',
+      'detection_classes'
+    ]:
         tensor_name = key + ':0'
         if tensor_name in all_tensor_names:
           tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
               tensor_name)
-      if 'detection_masks' in tensor_dict:
-        # The following processing is only for single image
-        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-            detection_masks, detection_boxes, image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(
-            tf.greater(detection_masks_reframed, 0.3), tf.uint8)
-        # Follow the convention by adding back the batch dimension
-        tensor_dict['detection_masks'] = tf.expand_dims(
-            detection_masks_reframed, 0)
-      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-      # Run inference
-      output_dict = sess.run(tensor_dict,
-                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+def squeeze_output (output_dict):
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+    output_dict['detection_classes'] = output_dict[
+      'detection_classes'][0].astype(np.uint8)
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+    return output_dict
 
-      # all outputs are float32 numpy arrays, so convert types as appropriate
-      output_dict['num_detections'] = int(output_dict['num_detections'][0])
-      output_dict['detection_classes'] = output_dict[
-          'detection_classes'][0].astype(np.uint8)
-      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-      output_dict['detection_scores'] = output_dict['detection_scores'][0]
-      if 'detection_masks' in output_dict:
-        output_dict['detection_masks'] = output_dict['detection_masks'][0]
-  return output_dict
-
-
-# In[106]:
-
+# Set up video reader and writer
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('car_detection.avi',fourcc, 20.0, (1280,720))
-
 cap = cv2.VideoCapture('test_video/video1080p.mp4')
-
 tracker_handler = TrackerHandler()
 counter = Counter()
-while (cap.isOpened()):
-  ret, image_np = cap.read()
-  if ret == False:
-      break
+cnt = 1
+pre_time = time.time()
 
-  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-  image_np_expanded = np.expand_dims(image_np, axis=0)
-  # Actual detection.
-  output_dict = run_inference_for_single_image(image_np, detection_graph)
+with detection_graph.as_default():
+    with tf.Session() as sess:
+        while (cap.isOpened()):
+          ret, image_np = cap.read()
+          if ret == False:
+              break
 
-  # Visualization of the results of a detection.
-  visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      tracker_handler = tracker_handler,
-      counter = counter,
-      instance_masks=output_dict.get('detection_masks'),
-      use_normalized_coordinates=True,
-      line_thickness=8,
-      skip_scores = True)
+          # Run inference
+          output_dict = sess.run(tensor_dict,
+                                 feed_dict={image_tensor: np.expand_dims(image_np, 0)})
+          output_dict = squeeze_output(output_dict)
 
-  out.write(image_np)
+          # Visualization of the results of a detection.
+          visualize_boxes_and_labels_on_image_array(
+              image_np,
+              output_dict['detection_boxes'],
+              output_dict['detection_classes'],
+              output_dict['detection_scores'],
+              category_index,
+              tracker_handler = tracker_handler,
+              counter = counter,
+              instance_masks=output_dict.get('detection_masks'),
+              use_normalized_coordinates=True,
+              line_thickness=4,
+              skip_scores = True,
+              hide_tracker_boxes=False)
 
-  cv2.imshow('Car Detection', image_np)
-  if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
+          out.write(image_np)
+
+          cv2.imshow('Car Detection', image_np)
+          if cv2.waitKey(25) & 0xFF == ord('q'):
+              break
+          #if cnt > 155:
+          #  time.sleep(1.5)
+          print ("frame", cnt, "second per frame:" , (time.time()-pre_time))
+          cnt += 1
+          pre_time = time.time()
 out.release()
 cap.release()
 cv2.destroyAllWindows()
